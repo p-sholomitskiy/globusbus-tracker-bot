af4676725893873baec93ad03827d8288e722ffe
Pavel Shalamitski <pshalamitski@supportheroes.io>
2026-04-07 07:30:10 +0300

    TSK-0002: added trip model

---
+++ src/models/trip.model.ts
@@ -0,0 +1,10 @@
+export type TripItem = {
+  id: string;
+  busNumber: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  startLocation: string;
+  endLocation: string;
+  availableSeats: number;
+};
